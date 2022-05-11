a9dea87737c9830c3e651a2a513f84e0b8a872ef
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-11 17:19:58 +0530

    Update App.js

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,6 +2,9 @@ import { Route ,Routes } from 'react-router-dom';
 import './App.css';
 import App1 from './components/App1';
 import App2 from './components/App2';
+import App3 from './components/App3';
+import {store} from './components/redux/store';
+import {Provider} from 'react-redux';
 
 
 
@@ -15,6 +18,9 @@ function App() {
       <Routes>
          <Route exact path='/' element={<App1 />}></Route>
          <Route exact path='/blog' element={<App2 />}></Route>
+         <Route exact path='/app3' element={
+         <Provider store={store}><App3/></Provider>
+         }></Route>
       </Routes>
         
         
