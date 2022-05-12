aeedeea5e54c15c5c0ebf1dcd0027a00a89c9a91
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-12 10:31:45 +0530

    Delete rootReducer.js

--- web-app/src/components/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { CALCULATER_KEY, calculatorReducer } from "./calculatorRedux/reducer";
-import { combineReducers } from "redux";
-
-let rootReducer = combineReducers({
-    [CALCULATER_KEY]: calculatorReducer
-})
-
-export {rootReducer}
