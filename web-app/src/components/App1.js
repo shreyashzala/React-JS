e831fd28afdb4f426bc776c9dc81545d3a632d9d
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-12 10:41:51 +0530

    Update App1.js

--- web-app/src/components/App1.js
+++ web-app/src/components/App1.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { List } from "./MovieList";
-import './css/App1.css';
+import { List } from "../containers/MovieList";
+import '../assets/App1.css';
 
 
 
@@ -11,32 +11,37 @@ export default function App1() {
     
     return (
         <>
-
+         <div className="head">
             {
                 showList.map((index) => {
                     return (
-                        <div key={index.id} className="row mt-3 mb-3" style={{border : "1px solid red"}}>
-                            <div className="col-md-6">
+                       
+
+                        
+                        <div key={index.id} className="list1 mt-3 mb-3" style={{border : "1px solid red"}}>
+                            
                                 <div className="p-1">
                                     <img src={index.img} />
                                 </div>
-                            </div>
-                            <div className="col-md-6 mt-3">
+
                                 <p><strong> Movie Name : </strong>{index.name}</p>
                                 <p>
                                  <strong>About:</strong>   {index.dis}</p>
                                 <p>Rating: <span className="font-weight-bold">{index.rating}/10</span></p>
                                 
-                            </div>
+                            
                             
                             <br />
                             
                             
                         </div>
+
+                    
                         
                     )
                 })
             }
+            </div>
         </>
     )
 }
