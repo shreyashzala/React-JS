41975f7ef60275e719646ba8fda6eb6764f1fe7a
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-12 10:33:00 +0530

    Create NavBar.js

---
+++ web-app/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-dark bg-dark">
+                <span className="navbar-brand mb-0 h1">Web App</span>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
