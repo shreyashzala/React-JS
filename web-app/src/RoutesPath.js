4bb181543fbdf09526286391be99a35ed726e4fd
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-18 11:09:58 +0530

    Update RoutesPath.js

--- web-app/src/RoutesPath.js
+++ web-app/src/RoutesPath.js
@@ -3,23 +3,24 @@ import { Route, Routes } from 'react-router-dom';
 import App1 from './components/App1';
 import App2 from './components/App2';
 import App3 from './components/App3';
-import Navbar from './components/Navbar';
 import {store} from './store/store';
 import {Provider} from 'react-redux';
+
 
 const RoutesPath = () => {
     return (
         <>
-            <Navbar />
-            <div className='container'>
+            
+            
                 <Routes>
                     <Route exact path='/' element={<App1 />}></Route>
                     <Route exact path='/blog' element={<App2 />}></Route>
                     <Route exact path='/app3' element={
                         <Provider store={store}><App3 /></Provider>
                     }></Route>
+                    
                 </Routes>
-            </div>
+            
         </>
     )
 }
