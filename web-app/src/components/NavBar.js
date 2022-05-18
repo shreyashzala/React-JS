6001ff8e0a7d0aaf3b789b99414c082303bc1d23
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-18 11:08:29 +0530

    Update NavBar.js

--- web-app/src/components/NavBar.js
+++ web-app/src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Navbar = () => {
+function Navbar(props) {
     return (
         <>
             <nav className="navbar navbar-dark bg-dark">
-                <span className="navbar-brand mb-0 h1">Web App</span>
+                <span className="navbar-brand mb-0 h1 text-light">{props.title}</span>
             </nav>
         </>
     )
