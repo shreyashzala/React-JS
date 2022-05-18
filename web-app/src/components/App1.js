194cbe69e39d00952d8535587ffd62e8e1a8b23c
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-18 11:05:15 +0530

    Update App1.js

--- web-app/src/components/App1.js
+++ web-app/src/components/App1.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { List } from "../containers/MovieList";
 import '../assets/App1.css';
+import Navbar from './Navbar';
+import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 
 
 
@@ -8,39 +10,45 @@ import '../assets/App1.css';
 export default function App1() {
 
     const [showList, setShowList] = useState(List);
-    
+
     return (
         <>
-         <div className="head">
-            {
-                showList.map((index) => {
-                    return (
-                       
+            <Navbar title="MovieList" />
 
-                        
-                        <div key={index.id} className="list1 mt-3 mb-3" style={{border : "1px solid red"}}>
-                            
-                                <div className="p-1">
-                                    <img src={index.img} />
-                                </div>
 
-                                <p><strong> Movie Name : </strong>{index.name}</p>
-                                <p>
-                                 <strong>About:</strong>   {index.dis}</p>
-                                <p>Rating: <span className="font-weight-bold">{index.rating}/10</span></p>
-                                
-                            
-                            
-                            <br />
-                            
-                            
-                        </div>
+            <br />
 
-                    
-                        
-                    )
-                })
-            }
+            <div className="head container">
+                <Grid container spacing={2}>
+                    {
+                        showList.map((index) => {
+                            return (
+                                <>
+                                    <Grid item md={6}>
+                                        <Card sx={{ maxWidth: 450 }} key={index.id}>
+                                            <CardMedia
+                                                component="img"
+                                                height={100}
+                                                image={index.img}
+                                                alt="img"
+                                            />
+                                            <CardContent>
+                                                <Typography variant='h5'  sx={{mb:1}}><strong>{index.name}</strong></Typography>
+                                                <Typography color='main.secondary'  sx={{mb:1}}><strong>About :</strong>
+                                                    {index.dis}
+                                                </Typography>
+                                                <Typography color='main.secondary'><strong>Rating : </strong>
+                                                    {index.rating}/10</Typography>
+                                            </CardContent>
+
+                                        </Card>
+                                    </Grid>
+                                </>
+                            )
+                        })
+
+                    }
+                </Grid>
             </div>
         </>
     )
