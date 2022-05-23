b1b36a12875883e594a7bc2116a5c0b5ed762711
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-05-23 12:41:55 +0530

    Create Questions.js

---
+++ web-app/src/containers/Questions.js
@@ -0,0 +1,47 @@
+export const Questions = [
+    {
+       id:1,
+       que: " Who is First Indian To win a Nobel Prize?",
+       optA: "J.R.D. Tata",
+       optB: "Rabidranath Tagore",
+       optC: "Dr. Hommie Bhabha",
+       optD: "Dr. C.V.Raman",
+       ans: "B"
+    },
+    {
+       id:2,
+       que: " Who is known the God Of Cricket?",
+       optA: "Virat Kohli",
+       optB: "Sachin Tendulkar",
+       optC: "MS Dhoni",
+       optD: "Rohit Sharma",
+       ans: "B"
+    },
+    {
+       id:3,
+       que: " Which is capital of India?",
+       optA: "Delhi",
+       optB: "Banglore",
+       optC: "Kolkata",
+       optD: "Mumbai",
+       ans: "A"
+    },
+    {
+       id:4,
+       que: " RBI Stands for?",
+       optA: "Rotation Bank Of India",
+       optB: "Roman Bank Of India",
+       optC: "Reserve Bank Of India",
+       optD: "Rular Bank Of India",
+       ans: "C"
+    },
+    {
+       id:5,
+       que: " Who was the first president of india?",
+       optA: "Rajendra Prasad",
+       optB: "Rajiv Gandhi",
+       optC: "Sardar Patel",
+       optD: "Javaharlal Nehru",
+       ans: "A"
+    }
+]
