b76ff813a4f3318d799f524b949a8d75c2a52809
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-06-03 13:48:08 +0530

    Create Hotels.js

---
+++ hotel-app/src/containers/Hotels.js
@@ -0,0 +1,194 @@
+export const Hotels = [
+    {
+        hotelname:"Nexus",
+        address: "350 M from Bus Station",
+        price:"1031",
+        rating:"4.2",
+        img : "Images/hotel/r-1.png",
+        city: "Rajkot"
+    },
+    {
+        hotelname:"Samrat International",
+        address: "440 M from Rajkot Bus Station",
+        price:"550",
+        rating:"4.1",
+        img : "Images/hotel/r-2.png",
+        city: "Rajkot"
+    },
+    {
+        hotelname:"Europe Inn",
+        address: "Near Rajkot Railway Station",
+        price:"1260",
+        rating:"4",
+        img : "Images/hotel/r-3.png",
+        city: "Rajkot"
+    },
+    {
+        hotelname:"Imperial",
+        address: "Bhakti Nagar",
+        price:"1500",
+        rating:"4.8",
+        img : "Images/hotel/r-4.png",
+        city: "Rajkot"
+    },
+    {
+        hotelname:"Platinum",
+        address: "670 M from Rajkot Airport",
+        price:"1600",
+        rating:"4.5",
+        img : "Images/hotel/r-5.png",
+        city: "Rajkot"
+    },
+    {
+        hotelname:"Silver Heights",
+        address: "Near Nehru Bridge",
+        price:"1608",
+        rating:"4.4",
+        img : "Images/hotel/a-1.png",
+        city: "Ahmedabad"
+    },
+    {
+        hotelname:"ELLOGA INN",
+        address: "Gota",
+        price:"2675",
+        rating:"4.4",
+        img : "Images/hotel/a-2.png",
+        city: "Ahmedabad"
+    },
+    {
+        hotelname:"Sarovar Portico Kalupar",
+        address: "Near Ahmedabad Railway Station",
+        price:"2192",
+        rating:"4.4",
+        img : "Images/hotel/a-3.png",
+        city: "Ahmedabad"
+    },
+    {
+        hotelname:"Pinnacle",
+        address: "near Nehru Bridge",
+        price:"1052",
+        rating:"3.9",
+        img : "Images/hotel/a-4.png",
+        city: "Ahmedabad"
+    },
+    {
+        hotelname:"German Palace, Airport",
+        address: "near Airport",
+        price:"2343",
+        rating:"3.5",
+        img : "Images/hotel/a-5.png",
+        city: "Ahmedabad"
+    },
+    {
+        hotelname:"Metro Palace",
+        address: "Bandra-West",
+        price:"3180",
+        rating:"3.8",
+        img : "Images/hotel/m-1.png",
+        city: "Mumbai"
+    },
+    {
+        hotelname:"Trident Bandra Kurla",
+        address: "near Mumbai Airport",
+        price:"1350",
+        rating:"4.6",
+        img : "Images/hotel/m-2.png",
+        city: "Mumbai"
+    },
+    {
+        hotelname:"Golden Palms",
+        address: "near Juhu Beach",
+        price:"2400",
+        rating:"4.4",
+        img : "Images/hotel/m-3.png",
+        city: "Mumbai"
+    },
+    {
+        hotelname:"Mina International",
+        address: "Jogeshwari West",
+        price:"1714",
+        rating:"3.8",
+        img : "Images/hotel/m-4.png",
+        city: "Mumbai"
+    },
+    {
+        hotelname:"Sista Andheri",
+        address: "Andheri",
+        price:"3166",
+        rating:"4.1",
+        img : "Images/hotel/m-5.png",
+        city: "Mumbai"
+    },
+    {
+        hotelname:"Grand Pragti",
+        address: "360 M from Airport",
+        price:"1875",
+        rating:"3.7",
+        img : "Images/hotel/b-1.png",
+        city: "Baroda"
+    },
+    {
+        hotelname:"King's Heritage",
+        address: "215 M from Baroda Train Tation",
+        price:"1100",
+        rating:"4",
+        img : "Images/hotel/b-2.png",
+        city: "Baroda"
+    },
+    {
+        hotelname:"Royal Rituals",
+        address: "750 M from Baroda Bus Tation",
+        price:"1700",
+        rating:"4.3",
+        img : "Images/hotel/b-3.png",
+        city: "Baroda"
+    },
+    {
+        hotelname:"Urban",
+        address: "Haripura",
+        price:"905",
+        rating:"4.2",
+        img : "Images/hotel/s-1.png",
+        city: "Surat"
+    },
+    {
+        hotelname:"Hospice",
+        address: "Khodiyar Nagar",
+        price:"1200",
+        rating:"4.1",
+        img : "Images/hotel/s-2.png",
+        city: "Surat"
+    },
+    {
+        hotelname:"Raj",
+        address: "Sumul Dairy Road",
+        price:"2880",
+        rating:"4.4",
+        img : "Images/hotel/s-3.png",
+        city: "Surat"
+    },
+    {
+        hotelname:"Leo Safforn",
+        address: "North Delhi",
+        price:"1378",
+        rating:"4.2",
+        img : "Images/hotel/d-1.png",
+        city: "Delhi"
+    },
+    {
+        hotelname:"Blue Bell",
+        address: "Karoli Bagh",
+        price:"1062",
+        rating:"3.9",
+        img : "Images/hotel/d-2.png",
+        city: "Delhi"
+    },
+    {
+        hotelname:"Toranto",
+        address: "Sadar Bazzar",
+        price:"857",
+        rating:"3.7",
+        img : "Images/hotel/d-3.png",
+        city: "Delhi"
+    },
+]
